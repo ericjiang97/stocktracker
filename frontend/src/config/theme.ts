4d73f9062551d78651ffacf074621fdf11b50cd9
Eric Jiang <ericjiang97@gmail.com>
2019-11-08 18:10:48 +1100

    [feat/style]: theme changes

--- frontend/src/config/theme.ts
+++ frontend/src/config/theme.ts
@@ -1,12 +1,11 @@
 import { createMuiTheme } from "@material-ui/core";
 import { ThemeOptions } from "@material-ui/core/styles/createMuiTheme";
-import { blueGrey } from "@material-ui/core/colors";
+import { blue, deepOrange } from "@material-ui/core/colors";
 
 export const theme: ThemeOptions = {
   palette: {
-    primary: {
-      main: blueGrey[500]
-    }
+    primary: blue,
+    secondary: deepOrange
   }
 };
 
