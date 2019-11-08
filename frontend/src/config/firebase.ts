933b3c30321519accdf3df3a5e6df1f3c7275c55
Eric Jiang <ericjiang97@gmail.com>
2019-11-08 19:59:39 +1100

    [feat]: firebase initial setup

---
+++ frontend/src/config/firebase.ts
@@ -0,0 +1,12 @@
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyB-RstX-iaM1cr7VPK5DyKisXuocFexm9g",
+  authDomain: "stocktracker-prod.firebaseapp.com",
+  databaseURL: "https://stocktracker-prod.firebaseio.com",
+  projectId: "stocktracker-prod",
+  storageBucket: "stocktracker-prod.appspot.com",
+  messagingSenderId: "777797430313",
+  appId: "1:777797430313:web:bcc43f72a24e8933990038",
+  measurementId: "G-GJHPFLT593"
+};
+
+export default FIREBASE_CONFIG;
---
+++ frontend/src/firebase/firebase.ts
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+
+import FIREBASE_CONFIG from "../config/firebase";
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(FIREBASE_CONFIG);
+}
+
+export const auth = firebase.auth();
+export const db = firebase.database();
