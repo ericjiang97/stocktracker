c521ff8e64dffced5312888b3893732108107e19
Eric Jiang <ericjiang97@gmail.com>
2019-11-08 18:24:57 +1100

    [ref/styles]: updating root component styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+
+import useStyles from "./styles";
 
 const App: React.FC = () => {
-  return <>Hello World!</>;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Switch>
+        <Route path="/" component={LandingPage} />
+      </Switch>
+    </div>
+  );
 };
 
 export default App;
---
+++ frontend/src/styles.ts
@@ -0,0 +1,11 @@
+import { makeStyles } from "@material-ui/core";
+
+const styles = makeStyles({
+  root: {
+    height: "100vh",
+    width: "100vw",
+    display: "flex"
+  }
+});
+
+export default styles;
