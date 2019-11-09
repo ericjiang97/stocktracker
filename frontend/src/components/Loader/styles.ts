19b1f86a5c2d49657add77bd5a5622911fbb8cb9
Eric Jiang <ericjiang97@gmail.com>
2019-11-09 11:54:07 +1100

    [feat/react]: Generic Loader component

---
+++ frontend/src/components/Loader/styles.ts
@@ -0,0 +1,11 @@
+import { makeStyles } from "@material-ui/styles";
+
+export const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
